85c3797e68a57b4de287adc8cb13404439092ed9
JeremyBarrera <112196500+JeremyBarrera@users.noreply.github.com>
2025-02-03 23:25:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = {
+    "♥" : "red" ,
+    "♣": "black" ,
+    "♠":"black",
+    "♦":"red",
+  }
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  function random(x){
+    return Math.floor(Math.random() * x)
+  }
+  function getValue(){
+    return random(values.length)
+  }
+  function getCard(){
+
+    let cardValue = getValue();
+    let clubKeys = Object.keys(suits);
+    let index = random(clubKeys.length);
+    let suit = clubKeys[index];
+    let color = suits[suit];
+
+    let cardItem = document.getElementById("item");
+    let clubLeft = document.getElementById("club-l");
+    let clubRight = document.getElementById("club-r");
+
+    cardItem.textContent = values[cardValue]
+    clubLeft.textContent = suit
+    clubRight.textContent = suit
+   
+    if (color === "red" ){
+      cardItem.style.color = "red"
+      clubLeft.style.color = "red"
+      clubRight.style.color = "red"
+    }
+    else{
+      cardItem.style.color = "black"
+      clubLeft.style.color = "black"
+      clubRight.style.color = "black"
+    }
+  }
+  getCard();
 };
